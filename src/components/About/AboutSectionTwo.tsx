1ba5a72aba21738217a2df30b068b1f94782b824
Hecze <hectorzerrillo85@gmail.com>
2024-06-06 16:35:45 -0500

    Update: About - new images

--- src/components/About/AboutSectionTwo.tsx
+++ src/components/About/AboutSectionTwo.tsx
@@ -23,18 +23,18 @@ const AboutSectionTwo = () => {
           <div className="w-full px-4 lg:w-1/2">
 
             <div
-              className="relative mx-auto mb-12 aspect-[25/24] max-w-[500px] text-center lg:m-0"
+              className="relative mx-auto mb-12 aspect-[25/24] max-w-[600px] text-center lg:m-0"
               data-wow-delay=".15s"
             >
               
               <Image
-                src="/images/about/about-image-2.svg"
+                src="/images/about/posits.svg"
                 alt="about image"
                 fill
                 className="drop-shadow-three dark:hidden dark:drop-shadow-none"
               />
               <Image
-                src="/images/about/about-image-2-dark.svg"
+                src="/images/about/posits.svg"
                 alt="about image"
                 fill
                 className="hidden drop-shadow-three dark:block dark:drop-shadow-none"
