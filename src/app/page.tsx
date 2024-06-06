a85a3f76025819a67b0972d1f7e5eb8179abf61c
Hecze <hectorzerrillo85@gmail.com>
2024-06-06 03:01:18 -0500

    se eliminaron las secciones remanentes no utilizadas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,10 @@
-import AboutSectionOne from "@/components/About/AboutSectionOne";
-
 import './globals.css';
-import AboutSectionTwo from "@/components/About/AboutSectionTwo";
-import Blog from "@/components/Blog";
 import Brands from "@/components/Brands";
 import Projects from "@/components/Projects";
 import ScrollUp from "@/components/Common/ScrollUp";
 import Contact from "@/components/Contact";
 import Features from "@/components/Features";
 import Hero from "@/components/Hero";
-import Pricing from "@/components/Pricing";
-import Testimonials from "@/components/Testimonials";
-import Video from "@/components/Video";
-import FadeInSection from "@/components/FadeInSection";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
