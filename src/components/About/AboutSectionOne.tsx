fb5f1158621a3e1a1353d7bda778ac72f8cb4794
Hecze <hectorzerrillo85@gmail.com>
2024-06-06 05:54:28 -0500

    About us

--- src/components/About/AboutSectionOne.tsx
+++ src/components/About/AboutSectionOne.tsx
@@ -1,23 +1,7 @@
 import Image from "next/image";
 import SectionTitle from "../Common/SectionTitle";
 
-
-const checkIcon = (
-  <svg width="16" height="13" viewBox="0 0 16 13" className="fill-current">
-    <path d="M5.8535 12.6631C5.65824 12.8584 5.34166 12.8584 5.1464 12.6631L0.678505 8.1952C0.483242 7.99994 0.483242 7.68336 0.678505 7.4881L2.32921 5.83739C2.52467 5.64193 2.84166 5.64216 3.03684 5.83791L5.14622 7.95354C5.34147 8.14936 5.65859 8.14952 5.85403 7.95388L13.3797 0.420561C13.575 0.22513 13.8917 0.225051 14.087 0.420383L15.7381 2.07143C15.9333 2.26669 15.9333 2.58327 15.7381 2.77854L5.8535 12.6631Z" />
-  </svg>
-);
-
 const AboutSectionOne = () => {
-  const List = ({ text }) => (
-    <p className="mb-5 flex items-center text-lg font-medium text-body-color">
-      <span className="mr-4 flex h-[30px] w-[30px] items-center justify-center rounded-md bg-primary bg-opacity-10 text-primary">
-        {checkIcon}
-      </span>
-      {text}
-    </p>
-  );
-
   return (
     <section id="about" className="pt-16 md:pt-20 lg:pt-28">
       <div className="container">
@@ -25,28 +9,25 @@ const AboutSectionOne = () => {
           <div className="-mx-4 flex flex-wrap items-center">
             <div className="w-full px-4 lg:w-1/2">
               <SectionTitle
-                title="section1.title"
-                paragraph="section1.paragraph"
+                title="Sobre Nosotros"
+                paragraph="Conoce más sobre nuestra empresa y nuestra visión."
                 mb="44px"
               />
 
-              <div
-                className="mb-12 max-w-[570px] lg:mb-0"
-                data-wow-delay=".15s"
-              >
-                <div className="mx-[-12px] flex flex-wrap">
-                  <div className="w-full px-3 sm:w-1/2 lg:w-full xl:w-1/2">
-                    <List text="section1.list.0" />
-                    <List text="section1.list.1" />
-                    <List text="section1.list.2" />
-                  </div>
+              <div className="mb-12 max-w-[570px] lg:mb-0" data-wow-delay=".15s">
+                <h3 className="mb-4 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">
+                  Quiénes Somos
+                </h3>
+                <p className="mb-5 text-lg font-medium text-body-color">
+                  Medula es una innovadora fábrica de software especializada en la creación de sitios web y aplicaciones móviles. Operamos bajo un modelo de Software como Servicio (SaaS), proporcionando soluciones innovadoras adaptadas a las necesidades únicas de nuestros clientes.
+                </p>
 
-                  <div className="w-full px-3 sm:w-1/2 lg:w-full xl:w-1/2">
-                    <List text="section1.list.3" />
-                    <List text="section1.list.4" />
-                    <List text="section1.list.5" />
-                  </div>
-                </div>
+                <h3 className="mb-4 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">
+                  Nuestro Enfoque Único
+                </h3>
+                <p className="mb-5 text-lg font-medium text-body-color">
+                  Lo que hace a Medula especial es nuestro pensamiento lateral y disruptivo. Reclutamos constantemente jóvenes talentos de universidades y concursos internacionales, invirtiendo en su formación y desarrollo. Este enfoque nos ha permitido construir un equipo con los mejores desarrolladores de Latinoamérica.
+                </p>
               </div>
             </div>
 
